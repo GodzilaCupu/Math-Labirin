55962005aa1c9528f54e0c8319335825411f8b1b
Gdz.Cp-Pc <eryparasetyo.15@gmail.com>
2020-12-15 12:55:21 +0700

    Adding Asset
    Asset From another game

---
+++ Assets/Script/Scoring.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Scoring : MonoBehaviour
+{
+    public GameObject[] scoreText;
+    public AudioSource audio;
+
+    private float x;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("TongOrganik"))
+        {
+            if (gameObject.CompareTag("Organik"))
+            {
+                Debug.Log("Berhasil");
+            }
+        }
+    }
+}
